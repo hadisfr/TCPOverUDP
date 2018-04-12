5638403f09ae9adba642081f9feee23b384ab7d6
h@di <hadi@hadisafari.ir>
2018-04-13 04:12:07 +0430

    make test verbose

--- src/Receiver.java
+++ src/Receiver.java
@@ -1,11 +1,12 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
+import java.nio.ByteBuffer;
 
 public class Receiver {
     public static void main(String[] args) throws Exception {
         TCPServerSocket tcpServerSocket = new TCPServerSocketImpl(12345);
         TCPSocket tcpSocket = tcpServerSocket.accept();
-        tcpSocket.receive("receiving.mp3");
+        tcpSocket.receiveAndLog("receiving.mp3");
         tcpSocket.close();
         tcpServerSocket.close();
     }
