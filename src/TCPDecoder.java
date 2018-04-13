56d3c0cadac4a09d280564e1876bc482b33919b6
h@di <hadi@hadisafari.ir>
2018-04-13 14:21:23 +0430

    fix minor bug

--- src/TCPDecoder.java
+++ src/TCPDecoder.java
@@ -1,3 +1,4 @@
+import java.nio.ByteBuffer;
 import java.util.*;
 
 public class TCPDecoder {
@@ -6,14 +7,24 @@ public class TCPDecoder {
         String str = scanner.next();
         byte[] data = new byte[str.length() / 2];
         for (int i = 0; i < str.length(); i += 2) {
-            data[i / 2] = (byte) ((Character.digit(str.charAt(i), 16) << 4) + (byte) Character.digit(str.charAt(i+1), 16));
+            data[i / 2] = (byte) ((Character.digit(str.charAt(i), 16) << 4)
+                    + (byte) Character.digit(str.charAt(i + 1), 16));
         }
         TCPPacket packet = new TCPPacket(data);
         System.out.println("sequenceNumber:\t\t" + packet.getSequenceNumber());
         System.out.println("acknowledgementNumber:\t" + packet.getAcknowledgementNumber());
         System.out.println("ACK:\t\t\t" + packet.getACK());
         System.out.println("SYN:\t\t\t" + packet.getSYN());
-        System.out.println("data (as Bytes):\t" + packet.getData());
+        System.out.println("data (as Hex):\t\t" + Integer.toHexString(toInt(packet.getData())));
         System.out.println("data (as String):\t" + new String(packet.getData()));
+    }
+
+    private static int toInt(byte[] bytes) {
+        int ret = 0;
+        for (byte b : bytes) {
+            ret <<= 8;
+            ret |= (int) b & 0xFF;
+        }
+        return ret;
     }
 }
